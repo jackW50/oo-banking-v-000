14f32ec13516ae5f54a95220bfa697077dc871b5
John Werenski <jtwerenski@gmail.com>
2018-09-29 16:04:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.status == "pending" && self.valid?
+    if self.status == "pending" && self.valid? && self.sender.balance >= self.amount
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
